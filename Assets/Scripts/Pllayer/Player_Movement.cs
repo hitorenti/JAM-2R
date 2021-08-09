dd0f2300e68a14b9b7a9a2e3a7f1f8dca9ab9a62
Hitorenti <zmilingrs@gmail.com>
2021-08-09 12:34:18 -0500

    Player Movement

--- Assets/Scripts/Pllayer/Player_Movement.cs
+++ Assets/Scripts/Pllayer/Player_Movement.cs
@@ -8,7 +8,11 @@ public class Player_Movement : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb2d;
-    private Animator anim;
+    private Animator anim; 
+    private bool IsJump;
+    public float RayDistance;
+    public float JumpForce;
+
 
     private void Awake()
     {
@@ -19,6 +23,8 @@ public class Player_Movement : MonoBehaviour
     private void Update()
     {
         //! Animations
+        anim.SetBool("jump", IsJump);
+
         if (rb2d.velocity.x > 0)
         {
             anim.SetBool("run", true);
@@ -40,20 +46,50 @@ public class Player_Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        // Draw ray for ground detection
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, RayDistance);
+        // debug ray
+        Debug.DrawRay(this.transform.position, Vector3.down * RayDistance, Color.red);
+
+        float XDir = Input.GetAxis("Horizontal");
 
         //! Raw for detect idle state
         if(Input.GetAxisRaw("Horizontal") == 0)
         {
             rb2d.velocity = Vector2.zero;
         }
-        else if(dir.x > 0)
+        else if(Input.GetKey(KeyCode.D))
         {
-            rb2d.velocity = new Vector2(dir.x * speed,rb2d.velocity.y);
+            rb2d.velocity = new Vector2(XDir * speed,rb2d.velocity.y);
         }
-        else if(dir.x < 0)
+        else if(Input.GetKey(KeyCode.A))
         {
-            rb2d.velocity = new Vector2((dir.x * speed), rb2d.velocity.y);
+            rb2d.velocity = new Vector2((XDir * speed), rb2d.velocity.y);
+        }
+
+        //? Is hit colliding?
+        if (hit.collider != null)
+        {
+
+            //? Is ground?
+            if (hit.collider.tag.Equals("GROUND"))
+            {
+                IsJump = false;
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.W) && !IsJump)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, JumpForce);
+            IsJump = true;
+            rb2d.gravityScale = 0;
+
+        }
+
+        if (IsJump)
+        {
+            rb2d.gravityScale = 1;
         }
 
 
